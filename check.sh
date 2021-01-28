5240e63e4aeafeb5142c42b1055d949ce6f89e24
federicoviscomi <federicoviscomi@gmail.com>
2021-01-28 21:38:37 +0000

    151

--- check.sh
+++ check.sh
@@ -2,34 +2,38 @@ curl -k -get https://ticketing.dev/api/users/currentuser
 echo
 echo
 
-
 response=$(
-curl -k\
-     --header "Content-Type: application/json" \
-     --request POST \
-     --data '{"email":"asdf","password":"sd"}' \
-     https://ticketing.dev/api/users/signup
+  curl -k \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{"email":"asdf","password":"sd"}' \
+    https://ticketing.dev/api/users/signup
 )
 
-jq --color-output <<< "${response}"
-
+jq --color-output <<<"${response}"
 
 response=$(
-curl -k\
-     --header "Content-Type: application/json" \
-     --request POST \
-     --data '{"email":"asdf@asdf.asdf","password":"dfdfdfd"}' \
-     https://ticketing.dev/api/users/signup
+  curl -k \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{"email":"asdf2@asdf.asdf","password":"dfdfdfd"}' \
+    https://ticketing.dev/api/users/signup
 )
 
-jq --color-output <<< "${response}"
+jq --color-output <<<"${response}"
 
+#second time
+response=$(
+  curl -k \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{"email":"asdf2@asdf.asdf","password":"dfdfdfd"}' \
+    https://ticketing.dev/api/users/signup
+)
+jq --color-output <<<"${response}"
 
 response=$(curl -k --request GET https://ticketing.dev/api/users/signup/asdfdfg)
-jq --color-output <<< "${response}"
+jq --color-output <<<"${response}"
 
 response=$(curl -k --request POST https://ticketing.dev/api/users/fafafafa)
-jq --color-output <<< "${response}"
-
-
-kubectl get pods
+jq --color-output <<<"${response}"
