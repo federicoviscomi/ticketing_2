69343913178603d2336fa2c94e80affb6eb7083d
federicoviscomi <federicoviscomi@gmail.com>
2021-02-02 16:18:44 +0000

    263

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,6 +6,8 @@ import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
 import {currentUser, errorHandler, NotFoundError} from '@procigatto/common';
 import {createTicketRouter} from './routes/new';
+import {showTicketRouter} from './routes/show';
+import {indexTicketRouter} from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,6 +22,10 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+
+app.use(showTicketRouter);
+
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
