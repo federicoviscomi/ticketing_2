d71d671855872106cce8f17d3105a27874fb000b
federicoviscomi <federicoviscomi@gmail.com>
2021-02-07 00:12:12 +0000

    330

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
+import {deleteOrderRouter} from './routes/delete';
+import {newOrderRouter} from './routes/new';
+import {showOrderRouter} from './routes/show';
+import {indexOrderRouter} from './routes/index';
+import {currentUser, errorHandler, NotFoundError} from "@procigatto/common";
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
