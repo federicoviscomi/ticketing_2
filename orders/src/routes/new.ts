5fed33dc31fc12bb730f28333e133530cb4a5cd8
federicoviscomi <federicoviscomi@gmail.com>
2021-02-07 18:40:41 +0000

    330

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,23 @@
 import express, {Request, Response} from 'express';
+import {requireAuth, validateRequest} from '@procigatto/common';
+import {body} from 'express-validator';
+import * as mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post(
+    '/api/orders',
+    requireAuth, [
+        body('ticket')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('ticket id must be provided')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        res.send({});
+    }
+);
 
 export {router as newOrderRouter};
