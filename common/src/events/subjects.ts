7d2382b09dcab9359c02ab8ea95d553d8f71a136
federicoviscomi <federicoviscomi@gmail.com>
2021-02-08 13:12:32 +0000

    357

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,7 +1,6 @@
 export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
-
   OrderCreated = 'order:craeted',
   OrderCancelled = 'order:cancelled'
 }
--- run.sh
+++ run.sh
@@ -5,20 +5,14 @@ echo
 echo -----------
 echo COMMON
 cd common
-UPDATE_COMMON="true"
-if ! git diff-index --quiet HEAD --; then
-  echo "changes in common"
-  npm install
-  git add .
-  git commit -m "updates" || true
-  npm version patch
-  npm run clean
-  npm run build
-  npm publish --access public
-  UPDATE_COMMON="true"
-else
-  echo "no changes in common"
-fi
+echo "changes in common"
+npm install
+git add .
+git commit -m "updates" || true
+npm version patch
+npm run clean
+npm run build
+npm publish --access public
 cd ../
 
 echo
@@ -26,13 +20,9 @@ echo
 echo -----------
 echo AUTH
 cd auth
-if [[ "${UPDATE_COMMON}" == "true" ]]; then
-  npm update @procigatto/common --save
-fi
-if ! git diff-index --quiet HEAD --; then
-  npm install
-  npm run test
-fi
+npm update @procigatto/common --save
+npm install
+npm run test
 cd ../
 
 echo
@@ -40,13 +30,9 @@ echo
 echo -----------
 echo TICKETS
 cd tickets
-if [[ "${UPDATE_COMMON}" == "true" ]]; then
-  npm update @procigatto/common --save
-fi
-if ! git diff-index --quiet HEAD --; then
-  npm install
-  npm run test
-fi
+npm update @procigatto/common --save
+npm install
+npm run test
 cd ../
 
 echo
@@ -54,13 +40,9 @@ echo
 echo -----------
 echo ORDERS
 cd orders
-if [[ "${UPDATE_COMMON}" == "true" ]]; then
-  npm update @procigatto/common --save
-fi
-if ! git diff-index --quiet HEAD --; then
-  npm install
-  npm run test
-fi
+npm update @procigatto/common --save
+npm install
+npm run test
 cd ../
 
 echo
@@ -68,12 +50,8 @@ echo
 echo -----------
 echo CLIENT
 cd client
-if [[ "${UPDATE_COMMON}" == "true" ]]; then
-  npm update @procigatto/common --save
-fi
-if ! git diff-index --quiet HEAD --; then
-  npm install
-fi
+npm update @procigatto/common --save
+npm install
 cd ../
 
 echo
@@ -81,12 +59,8 @@ echo
 echo -----------
 echo NATS TEST
 cd nats-test
-if [[ "${UPDATE_COMMON}" == "true" ]]; then
-  npm update @procigatto/common --save
-fi
-if ! git diff-index --quiet HEAD --; then
-  npm install
-fi
+npm update @procigatto/common --save
+npm install
 cd ../
 
 docker-compose build
